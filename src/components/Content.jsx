9290101fc40698a5274ec1e1993d58c1eaaa2c2c
diego <diego_6525@hotmail.com>
2020-05-11 17:44:21 -0700

    refactoring

---
+++ src/components/Content.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+function Content({ response }) {
+  const content = response ? (
+    response["statusCode"] !== 400 ? (
+      <div>
+        <img src={response["thumbnail_url"]}></img>
+        <p>{response["name"]}</p>
+        <p>
+          Highest Weekly Mythic Plus{" "}
+          {response["mythic_plus_weekly_highest_level_runs"][0]["dungeon"]}
+          {" " +
+            response["mythic_plus_weekly_highest_level_runs"][0][
+              "mythic_level"
+            ]}
+        </p>
+      </div>
+    ) : (
+      <p>Could not find character</p>
+    )
+  ) : (
+    <p></p>
+  );
+
+  return <div>{content}</div>;
+}
+
+export default Content;
