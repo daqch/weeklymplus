312a31712df6fb8920d9aa32d6c839a8d3d3f3e5
diego <diego_6525@hotmail.com>
2020-08-02 04:09:44 -0700

    merged server and client repos

---
+++ client/build/precache-manifest.4c3362b77fcfeda6fd210c785838e15b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8926e7fc70f2121c6aa9884c5d4e2ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34ba715199e3fad99a0c",
+    "url": "/static/css/main.fed161b2.chunk.css"
+  },
+  {
+    "revision": "795c37b85f9e5ba94a22",
+    "url": "/static/js/2.77824e33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77824e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ba715199e3fad99a0c",
+    "url": "/static/js/main.aab4212f.chunk.js"
+  },
+  {
+    "revision": "bd992d8fbd39b0ee5039",
+    "url": "/static/js/runtime-main.500170e0.js"
+  },
+  {
+    "revision": "c2330e14a2ba4cf9fdba2910374e9271",
+    "url": "/static/media/finalchest.pgn.c2330e14.png"
+  }
+]);
