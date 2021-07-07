e1244de722fd934145646a46f40f4ba64d359322
diego <diegoaquinochavez@uvic.ca>
2021-07-07 05:30:58 -0700

    new new db.js

---
+++ db.js
@@ -0,0 +1,9 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOSTNAME,
+    port: process.env.DB_PORT,
+    database: process.env.DB
+})
